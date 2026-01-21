db1e78e444f27e4bcf33f37d59ca32bd73197a4b
Foote, Brian <footeb@lakeland.edu>
2026-01-21 13:41:45 -0600

    checkin

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using BDF.DVDCentral.PL.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program
+{
+    //private static SiteService _siteService;
+    private static DVDCentralEntities dc;
+
+    private static void Main(string[] args)
+    {
+        var services = new ServiceCollection();
+        services.AddDbContext<DVDCentralEntities>(options => options.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=BDF.DVDCentral.DB;Integrated Security=True"));
+
+        //builder.Services.AddDbContextPool<DVDCentralEntities>(options =>
+        //options.UseSqlServer(builder.Configuration.GetConnectionString("DVDCentralConnection"), builder =>
+        //{
+        //    builder.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+        //}
+        //));
+
+        var serviceProvider = services.BuildServiceProvider();
+        //_siteService = serviceProvider.GetService<SiteService>();
+        dc = serviceProvider.GetService<DVDCentralEntities>();
+
+        //DVDCentralEntities dc = services.BuildServiceProvider().GetRequiredService<DVDCentralEntities>();
+        //var movies = dc.tblMovies.ToList();
+        //movies.ForEach(m => Console.WriteLine(m.Title));
+        //Console.WriteLine("Hello, World!");
+        //Console.ReadLine();
+    }
+}
