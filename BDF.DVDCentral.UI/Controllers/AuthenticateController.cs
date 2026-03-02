1e4cc90c129b119ae550b61efe438e03d0af42cb
Foote, Brian D <120212964@fvtc.edu>
2026-03-02 14:21:40 -0600

    Added autheticate

---
+++ BDF.DVDCentral.UI/Controllers/AuthenticateController.cs
@@ -0,0 +1,25 @@
+﻿using BDF.DVDCentral.UI.Models;
+using Microsoft.AspNetCore.Http.Extensions;
+//using Microsoft.AspNetCore.Mvc;
+
+namespace BDF.DVDCentral.UI.Controllers
+{
+    public class AuthenticateController : Controller
+    {
+        public IActionResult AuthenticateUser()
+        {
+            if (!Authenticate.IsAuthenticated(HttpContext))
+            {
+                return RedirectToAction("Login",
+                                        "User",
+                                        new
+                                        {
+                                            returnUrl = (HttpContext?.Request == null ? "/"
+                                        : UriHelper.GetDisplayUrl(HttpContext.Request))
+                                        });
+
+            }
+            return null!;   // Is authenticated...continue on.
+        }
+    }
+}
