67b38d712ea8e5dcada28198f2b00ad4f54e0b00
Foote, Brian D <120212964@fvtc.edu>
2026-03-04 14:31:47 -0600

    Added BDF.LunchOrder.Web

---
+++ BDF.LunchOrder.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BDF.LunchOrder.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
